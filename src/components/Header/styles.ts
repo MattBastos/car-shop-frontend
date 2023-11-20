3b27d627a8432ccd2598e86fdfd954f7d4864b66
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-20 13:12:05 -0300

    feat: Implements Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,13 @@
+import tw from 'tailwind-styled-components';
+
+export const Container = tw.header`
+  fixed
+  z-40
+  flex
+  h-16
+  w-full
+  items-center
+  justify-center
+  bg-gray-200
+  shadow-xl
+`;
