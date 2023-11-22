556a988ec0c8718601e761aa0a23864caee0907b
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-22 10:51:30 -0300

    feat: implements CarTable

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,7 +1,10 @@
+import { CarTable } from '@/components/Tables';
+
 export default function Cars() {
   return (
-    <section>
-      <h1 className="text-white">Carros</h1>
+    <section className="flex flex-col items-center justify-center gap-5">
+      <h1 className="text-4xl font-bold text-gray-200">Carros</h1>
+      <CarTable />
     </section>
   );
 }
--- src/components/Tables/CarTable/CarTable.tsx
+++ src/components/Tables/CarTable/CarTable.tsx
@@ -7,6 +7,10 @@ import { useState, useEffect } from 'react';
 export const CarTable = () => {
   const [carsData, setCarsData] = useState<Car[]>([]);
 
+  const onDelete = () => {};
+
+  const onUpdate = () => {};
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await getCars();
@@ -21,7 +25,43 @@ export const CarTable = () => {
 
   return (
     <section>
-      <h1>{carsData[0].model}</h1>
+      {carsData.length === 0 ? (
+        <p>Nenhum carro encontrado!</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Model</th>
+              <th>Year</th>
+              <th>Color</th>
+              <th>Status</th>
+              <th>Buy Value</th>
+              <th>Doors Quantity</th>
+              <th>Seats Quantity</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {carsData.map((car) => (
+              <tr key={car.id}>
+                <td>{car.id}</td>
+                <td>{car.model}</td>
+                <td>{car.year}</td>
+                <td>{car.color}</td>
+                <td>{car.status ? 'Active' : 'Inactive'}</td>
+                <td>{car.buyValue}</td>
+                <td>{car.doorsQty}</td>
+                <td>{car.seatsQty}</td>
+                <td>
+                  <button onClick={() => onDelete()}>Deletar</button>
+                  <button onClick={() => onUpdate()}>Editar</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </section>
   );
 };
