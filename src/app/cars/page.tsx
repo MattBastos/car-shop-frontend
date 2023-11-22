4d9f88dcbd73ff283593000ea35a16378098a846
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-22 15:59:20 -0300

    style: Add responsiveness to CarTable component

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,10 +1,5 @@
 import { CarTable } from '@/components/Tables';
 
 export default function Cars() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-5">
-      <h1 className="text-4xl font-bold text-gray-200">Carros</h1>
-      <CarTable />
-    </section>
-  );
+  return <CarTable />;
 }
--- src/components/Tables/CarTable/CarTable.tsx
+++ src/components/Tables/CarTable/CarTable.tsx
@@ -6,6 +6,17 @@ import { useState, useEffect } from 'react';
 
 export const CarTable = () => {
   const [carsData, setCarsData] = useState<Car[]>([]);
+  const tableHeaders = [
+    'ID',
+    'Modelo',
+    'Ano',
+    'Cor',
+    'Disponibilidade',
+    'Valor',
+    'Portas',
+    'Assentos',
+    'Ações'
+  ];
 
   const onDelete = () => {};
 
@@ -14,69 +25,74 @@ export const CarTable = () => {
   useEffect(() => {
     const fetchData = async () => {
       const data = await getCars();
-      if (data) {
-        setCarsData(data);
-        console.log(data);
-      }
+      if (data) setCarsData(data);
     };
 
     fetchData();
   }, []);
 
   return (
-    <section>
+    <section className="mx-auto flex min-w-full flex-col items-center justify-center gap-5">
+      <h1 className="text-4xl font-bold text-gray-100">Carros</h1>
+
       {carsData.length === 0 ? (
         <p>Nenhum carro encontrado!</p>
       ) : (
-        <table className="min-w-full rounded-lg bg-gray-200">
-          <thead>
-            <tr className="text-center text-lg text-gray-800">
-              <th className="border-b border-gray-950 px-4 py-2">ID</th>
-              <th className="border-b border-gray-950 px-4 py-2">Modelo</th>
-              <th className="border-b border-gray-950 px-4 py-2">Ano</th>
-              <th className="border-b border-gray-950 px-4 py-2">Cor</th>
-              <th className="border-b border-gray-950 px-4 py-2">Status</th>
-              <th className="border-b border-gray-950 px-4 py-2">Valor</th>
-              <th className="border-b border-gray-950 px-4 py-2">Portas</th>
-              <th className="border-b border-gray-950 px-4 py-2">Assentos</th>
-              <th className="border-b border-gray-950 px-4 py-2">Ações</th>
-            </tr>
-          </thead>
+        <section className="relative overflow-x-auto rounded-xl">
+          <section className="overflow-x-hidden rounded-xl">
+            <table className="w-full table-auto border-collapse border-spacing-0 rounded-xl bg-gray-200 text-center">
+              <thead className="bg-primary text-base uppercase text-gray-200">
+                <tr>
+                  {tableHeaders.map((th) => (
+                    <th scope="col" key={th} className="px-2 py-3">
+                      {th}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
 
-          <tbody>
-            {carsData.map((car) => (
-              <tr
-                key={car.id}
-                className="text-center text-lg text-gray-800 transition-colors ease-in-out hover:bg-gray-300"
-              >
-                <td className="px-4 py-2">{car.id}</td>
-                <td className="px-4 py-2">{car.model}</td>
-                <td className="px-4 py-2">{car.year}</td>
-                <td className="px-4 py-2">{car.color}</td>
-                <td className="px-4 py-2">
-                  {car.status ? 'Disponível' : 'Não Disponível'}
-                </td>
-                <td className="px-4 py-2">{car.buyValue}</td>
-                <td className="px-4 py-2">{car.doorsQty}</td>
-                <td className="px-4 py-2">{car.seatsQty}</td>
-                <td className="flex gap-4 px-4 py-2">
-                  <button
-                    onClick={onUpdate}
-                    className="rounded-lg border bg-blue-500 px-2 py-1 text-white transition-colors duration-200 ease-in-out hover:bg-blue-600"
+              <tbody className="text-base text-gray-800">
+                {carsData.map((car) => (
+                  <tr
+                    key={car.id}
+                    className="border-b border-gray-400 transition-colors ease-in-out hover:bg-gray-300"
                   >
-                    Editar
-                  </button>
-                  <button
-                    onClick={onDelete}
-                    className="rounded-lg bg-red-600 px-2 py-1 text-white transition-colors ease-in-out hover:bg-red-700"
-                  >
-                    Deletar
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                    <td className="px-3 py-2">{car.id}</td>
+                    <td className="px-3 py-2">{car.model}</td>
+                    <td className="px-3 py-2">{car.year}</td>
+                    <td className="px-3 py-2">{car.color}</td>
+                    <td
+                      className={
+                        car.status
+                          ? 'px-3 py-2 text-green-700'
+                          : 'px-3 py-2 text-red-700'
+                      }
+                    >
+                      {car.status ? 'Disponível' : 'Não Disponível'}
+                    </td>
+                    <td className="px-3 py-2">{car.buyValue}</td>
+                    <td className="px-3 py-2">{car.doorsQty}</td>
+                    <td className="px-3 py-2">{car.seatsQty}</td>
+                    <td className="flex gap-4 px-3 py-2">
+                      <button
+                        onClick={onUpdate}
+                        className="rounded-lg bg-blue-500 px-2 py-1 text-white transition-colors duration-200 ease-in-out hover:bg-blue-600"
+                      >
+                        Editar
+                      </button>
+                      <button
+                        onClick={onDelete}
+                        className="rounded-lg bg-red-500 px-2 py-1 text-white transition-colors duration-200 ease-in-out hover:bg-red-600"
+                      >
+                        Deletar
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </section>
+        </section>
       )}
     </section>
   );
