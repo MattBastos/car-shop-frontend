b643c43b9c5a3b3e00bd64f305e19a01a9faf4ae
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-22 11:29:34 -0300

    style: Add CarTable styles

--- src/components/Tables/CarTable/CarTable.tsx
+++ src/components/Tables/CarTable/CarTable.tsx
@@ -28,34 +28,50 @@ export const CarTable = () => {
       {carsData.length === 0 ? (
         <p>Nenhum carro encontrado!</p>
       ) : (
-        <table>
+        <table className="min-w-full rounded-lg bg-gray-200">
           <thead>
-            <tr>
-              <th>ID</th>
-              <th>Model</th>
-              <th>Year</th>
-              <th>Color</th>
-              <th>Status</th>
-              <th>Buy Value</th>
-              <th>Doors Quantity</th>
-              <th>Seats Quantity</th>
-              <th>Actions</th>
+            <tr className="text-center text-lg text-gray-800">
+              <th className="border-b border-gray-950 px-4 py-2">ID</th>
+              <th className="border-b border-gray-950 px-4 py-2">Modelo</th>
+              <th className="border-b border-gray-950 px-4 py-2">Ano</th>
+              <th className="border-b border-gray-950 px-4 py-2">Cor</th>
+              <th className="border-b border-gray-950 px-4 py-2">Status</th>
+              <th className="border-b border-gray-950 px-4 py-2">Valor</th>
+              <th className="border-b border-gray-950 px-4 py-2">Portas</th>
+              <th className="border-b border-gray-950 px-4 py-2">Assentos</th>
+              <th className="border-b border-gray-950 px-4 py-2">Ações</th>
             </tr>
           </thead>
+
           <tbody>
             {carsData.map((car) => (
-              <tr key={car.id}>
-                <td>{car.id}</td>
-                <td>{car.model}</td>
-                <td>{car.year}</td>
-                <td>{car.color}</td>
-                <td>{car.status ? 'Active' : 'Inactive'}</td>
-                <td>{car.buyValue}</td>
-                <td>{car.doorsQty}</td>
-                <td>{car.seatsQty}</td>
-                <td>
-                  <button onClick={() => onDelete()}>Deletar</button>
-                  <button onClick={() => onUpdate()}>Editar</button>
+              <tr
+                key={car.id}
+                className="text-center text-lg text-gray-800 transition-colors ease-in-out hover:bg-gray-300"
+              >
+                <td className="px-4 py-2">{car.id}</td>
+                <td className="px-4 py-2">{car.model}</td>
+                <td className="px-4 py-2">{car.year}</td>
+                <td className="px-4 py-2">{car.color}</td>
+                <td className="px-4 py-2">
+                  {car.status ? 'Disponível' : 'Não Disponível'}
+                </td>
+                <td className="px-4 py-2">{car.buyValue}</td>
+                <td className="px-4 py-2">{car.doorsQty}</td>
+                <td className="px-4 py-2">{car.seatsQty}</td>
+                <td className="flex gap-4 px-4 py-2">
+                  <button
+                    onClick={onUpdate}
+                    className="rounded-lg border bg-yellow-500 px-2 py-1 text-white transition-colors ease-in-out hover:bg-yellow-600"
+                  >
+                    Editar
+                  </button>
+                  <button
+                    onClick={onDelete}
+                    className="rounded-lg bg-red-600 px-2 py-1 text-white transition-colors ease-in-out hover:bg-red-700"
+                  >
+                    Deletar
+                  </button>
                 </td>
               </tr>
             ))}
