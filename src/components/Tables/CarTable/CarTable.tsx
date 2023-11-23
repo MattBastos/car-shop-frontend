ea3c30a7694f731271ea6e9b8671123690dffe8a
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-23 10:38:45 -0300

    style: Add styled components to CarTable component

--- src/components/Tables/CarTable/CarTable.tsx
+++ src/components/Tables/CarTable/CarTable.tsx
@@ -4,6 +4,8 @@ import { getCars } from '@/api/cars';
 import { Car } from '@/types';
 import { useState, useEffect } from 'react';
 
+import * as S from './styles';
+
 export const CarTable = () => {
   const [carsData, setCarsData] = useState<Car[]>([]);
   const tableHeaders = [
@@ -31,57 +33,58 @@ export const CarTable = () => {
   }, []);
 
   return carsData.length === 0 ? (
-    <p>Nenhum carro encontrado!</p>
+    <S.NoCarMessage>Nenhum carro encontrado!</S.NoCarMessage>
   ) : (
-    <section className="relative w-full overflow-x-auto rounded-t-xl">
-      <table className="w-full table-auto border-collapse border-spacing-0 rounded-t-xl bg-gray-200 text-center">
-        <thead className="bg-primary text-sm uppercase text-gray-200 md:text-base lg:text-lg">
+    <S.Container>
+      <S.Table>
+        <S.THead>
           <tr>
             {tableHeaders.map((th) => (
-              <th scope="col" key={th} className="px-4 py-3">
+              <S.TH scope="col" key={th}>
                 {th}
-              </th>
+              </S.TH>
             ))}
           </tr>
-        </thead>
+        </S.THead>
 
-        <tbody className="text-sm text-gray-800 md:text-base lg:text-lg">
+        <S.TBody>
           {carsData.map((car) => (
-            <tr
-              key={car.id}
-              className="border-b border-gray-400 transition-colors ease-in-out hover:bg-gray-300"
-            >
-              <td className="px-2 py-2 md:px-4">{car.model}</td>
-              <td className="px-2 py-2 md:px-4">{car.year}</td>
-              <td className="px-2 py-2 md:px-4">{car.color}</td>
-              <td
-                className={`px-2 py-2 md:px-4 ${
-                  car.status ? 'text-green-700' : 'text-red-700'
-                }`}
-              >
+            <S.TBodyRow key={car.id}>
+              <S.TD>{car.model}</S.TD>
+
+              <S.TD>{car.year}</S.TD>
+
+              <S.TD>{car.color}</S.TD>
+
+              <S.TD className={car.status ? 'text-green-700' : 'text-red-700'}>
                 {car.status ? 'Disponível' : 'Não Disponível'}
-              </td>
-              <td className="px-2 py-2 md:px-4">{car.buyValue}</td>
-              <td className="px-2 py-2 md:px-4">{car.doorsQty}</td>
-              <td className="px-2 py-2 md:px-4">{car.seatsQty}</td>
-              <td className="flex items-center justify-center gap-4 px-2 py-2 md:px-4">
-                <button
+              </S.TD>
+
+              <S.TD>{car.buyValue}</S.TD>
+
+              <S.TD>{car.doorsQty}</S.TD>
+
+              <S.TD>{car.seatsQty}</S.TD>
+
+              <S.TDActions>
+                <S.TButton
                   onClick={onUpdate}
-                  className="rounded-lg bg-blue-500 px-2 py-1 text-white transition-colors duration-200 ease-in-out hover:bg-blue-600"
+                  className="bg-blue-500 hover:bg-blue-600"
                 >
                   Editar
-                </button>
-                <button
+                </S.TButton>
+
+                <S.TButton
                   onClick={onDelete}
-                  className="rounded-lg bg-red-500 px-2 py-1 text-white transition-colors duration-200 ease-in-out hover:bg-red-600"
+                  className="bg-red-500 hover:bg-red-600"
                 >
                   Deletar
-                </button>
-              </td>
-            </tr>
+                </S.TButton>
+              </S.TDActions>
+            </S.TBodyRow>
           ))}
-        </tbody>
-      </table>
-    </section>
+        </S.TBody>
+      </S.Table>
+    </S.Container>
   );
 };
