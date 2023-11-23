046a983c09946b356725669344e7d027d68b9d90
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-23 09:33:07 -0300

    style: Add responsiveness to CarTable component

--- src/components/Tables/CarTable/CarTable.tsx
+++ src/components/Tables/CarTable/CarTable.tsx
@@ -32,7 +32,7 @@ export const CarTable = () => {
 
   return (
     <section className="mx-auto flex min-w-full flex-col items-center justify-center gap-5">
-      <h1 className="text-4xl font-bold text-gray-100">Carros</h1>
+      <h1 className="text-2xl font-bold text-gray-100 md:text-4xl">Carros</h1>
 
       {carsData.length === 0 ? (
         <p>Nenhum carro encontrado!</p>
@@ -40,38 +40,38 @@ export const CarTable = () => {
         <section className="relative overflow-x-auto rounded-xl">
           <section className="overflow-x-hidden rounded-xl">
             <table className="w-full table-auto border-collapse border-spacing-0 rounded-xl bg-gray-200 text-center">
-              <thead className="bg-primary text-base uppercase text-gray-200">
+              <thead className="bg-primary text-sm uppercase text-gray-200 md:text-base">
                 <tr>
                   {tableHeaders.map((th) => (
-                    <th scope="col" key={th} className="px-4 py-3">
+                    <th scope="col" key={th} className="px-2 py-3 md:px-4">
                       {th}
                     </th>
                   ))}
                 </tr>
               </thead>
 
-              <tbody className="text-base text-gray-800">
+              <tbody className="text-sm text-gray-800 md:text-base">
                 {carsData.map((car) => (
                   <tr
                     key={car.id}
                     className="border-b border-gray-400 transition-colors ease-in-out hover:bg-gray-300"
                   >
-                    <td className="px-4 py-2">{car.model}</td>
-                    <td className="px-4 py-2">{car.year}</td>
-                    <td className="px-4 py-2">{car.color}</td>
+                    <td className="px-2 py-2 md:px-4">{car.model}</td>
+                    <td className="px-2 py-2 md:px-4">{car.year}</td>
+                    <td className="px-2 py-2 md:px-4">{car.color}</td>
                     <td
                       className={
                         car.status
-                          ? 'px-3 py-4 text-green-700'
-                          : 'px-3 py-4 text-red-700'
+                          ? 'px-2 py-2 text-green-700 md:px-4'
+                          : 'px-2 py-2 text-red-700 md:px-4'
                       }
                     >
                       {car.status ? 'Disponível' : 'Não Disponível'}
                     </td>
-                    <td className="px-4 py-2">{car.buyValue}</td>
-                    <td className="px-4 py-2">{car.doorsQty}</td>
-                    <td className="px-4 py-2">{car.seatsQty}</td>
-                    <td className="flex gap-4 px-4 py-2">
+                    <td className="px-2 py-2 md:px-4">{car.buyValue}</td>
+                    <td className="px-2 py-2 md:px-4">{car.doorsQty}</td>
+                    <td className="px-2 py-2 md:px-4">{car.seatsQty}</td>
+                    <td className="flex gap-4 px-2 py-2 md:px-4">
                       <button
                         onClick={onUpdate}
                         className="rounded-lg bg-blue-500 px-2 py-1 text-white transition-colors duration-200 ease-in-out hover:bg-blue-600"
