94234b3d25c19e7baeff2cc2a90f521bbc8b0d31
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-23 09:40:33 -0300

    feat: Implements SectionTitle component

--- src/components/Section/SectionTitle/SectionTitle.tsx
+++ src/components/Section/SectionTitle/SectionTitle.tsx
@@ -1,1 +1,9 @@
-export const SectionTitle = () => {};
+import * as S from './styles';
+
+type SectionTitleProps = {
+  title: string;
+};
+
+export const SectionTitle = ({ title }: SectionTitleProps) => {
+  return <S.Title>{title}</S.Title>;
+};
---
+++ src/components/Section/SectionTitle/styles.ts
@@ -0,0 +1,8 @@
+import tw from 'tailwind-styled-components';
+
+export const Title = tw.h1`
+  text-2xl
+  font-bold
+  text-gray-100
+  md:text-4xl
+`;
