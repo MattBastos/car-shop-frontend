7803a59565e1b38e140e0b58f7ea17f701bc445d
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-24 10:44:37 -0300

    style: Apply responsiveness to TableButton

--- src/components/Tables/TableButton/styles.ts
+++ src/components/Tables/TableButton/styles.ts
@@ -1,9 +1,18 @@
 import tw from 'tailwind-styled-components';
 
 export const Button = tw.button`
-  rounded-lg
-  px-2
-  py-1
+  flex
+  items-center
+  justify-center
+  h-7
+  w-14
+  md:h-9
+  md:w-16
+  lg:h-10
+  lg:w-20
+  p-2
+  rounded-md
+  md:rounded-lg
   text-white
   transition-colors
   duration-200
