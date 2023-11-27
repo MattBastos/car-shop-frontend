69b25dbd3d718b3cc6a25d62e5461c57f7046374
Matthews Bastos <matthewsbasttos@gmail.com>
2023-11-27 11:30:27 -0300

    feat: Creates editCar function

--- src/api/cars.ts
+++ src/api/cars.ts
@@ -5,7 +5,7 @@ export const getCars = async () => {
     const { data } = await axiosInstance.get('cars');
     if (data) return data;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error(`Erro ao buscar dados dos carros: ${error}`);
   }
 };
 
@@ -16,6 +16,17 @@ export const deleteCar = async (carId: string) => {
       return 'Carro deletado com sucesso!';
     }
   } catch (error) {
-    `Erro ao deletar carro:, ${error}`;
+    `Erro ao deletar carro: ${error}`;
+  }
+};
+
+export const editCar = async (carId: string) => {
+  try {
+    const { status } = await axiosInstance.put(`cars/${carId}`);
+    if (status === 200) {
+      return 'Carro editado com sucesso!';
+    }
+  } catch (error) {
+    `Erro ao editar carro: ${error}`;
   }
 };
