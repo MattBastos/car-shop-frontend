759019cd8bbc68591827d71eecbaa47f42dadf67
Matthews Bastos <matthewsbasttos@gmail.com>
2023-12-06 13:34:35 -0300

    feat: Creates deleteMotorcycle method

--- src/api/motorcycles.ts
+++ src/api/motorcycles.ts
@@ -10,3 +10,17 @@ export const getMotorcycles = async () => {
     console.error(`Erro ao buscar dados das motocicletas: ${error}`);
   }
 };
+
+export const deleteMotorcycle = async (motorcycleId: string) => {
+  try {
+    const { status } = await axiosInstance.delete(
+      `motorcycles/${motorcycleId}`
+    );
+
+    if (status === 200) {
+      return 'Motocicleta deletada com sucesso!';
+    }
+  } catch (error) {
+    `Erro ao deletar motocicleta: ${error}`;
+  }
+};
