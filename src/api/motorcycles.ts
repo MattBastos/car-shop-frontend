d03df8439ce34b60cd853f0f7127fe84020ef1b5
Matthews Bastos <matthewsbasttos@gmail.com>
2023-12-06 13:33:12 -0300

    feat: Creates getMotorcycles method

--- src/api/motorcycles.ts
+++ src/api/motorcycles.ts
@@ -1,3 +1,12 @@
 import { Motorcycle } from '@/types';
 
 import { axiosInstance } from './axiosConfig';
+
+export const getMotorcycles = async () => {
+  try {
+    const { data } = await axiosInstance.get('motorcycles');
+    if (data) return data;
+  } catch (error) {
+    console.error(`Erro ao buscar dados das motocicletas: ${error}`);
+  }
+};
